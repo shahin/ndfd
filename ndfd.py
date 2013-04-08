71dc013cf236da23937f562fb84114ea51ef341e
Shahin Saneinejad <shahin.saneinejad@gmail.com>
2013-04-07 17:25:30 -0700

    Refactor as a class and add interpolation test.

--- ndfd.py
+++ ndfd.py
@@ -1,86 +1,146 @@
 import requests
 import lxml.etree as etree
+
 import datetime
 import dateutil.parser as dp
 import pytz
 
+import logging
 
-def download_forecast():
-  # download forecast data
+
+class NDFD:
+  '''
+  A lightweight client for the NDFD REST web service.
+  
+  Currently supports the following forecast elements: 
+    - probability-of-precipitation (POP12)
+
+  NDFD REST: http://graphical.weather.gov/xml/rest.php
+  '''
+
+  ELEMENT_ABBR = {
+      'probability-of-precipitation': 'pop12'
+      }
 
   FORECAST_URL = ("http://www.weather.gov/forecasts/xml"
     "/sample_products/browser_interface"
     "/ndfdXMLclient.php")
 
-  lat = 37.76
-  lon = -122.412
-  start_date = datetime.datetime.today()
-  num_days = 2
-  metric = True
-  location_info = [("lat", lat), ("lon", lon)]
-  params = location_info + [("begin", start_date.strftime("%Y-%m-%dT%H:%M")),
-                            ("end", (start_date + datetime.timedelta(num_days)).strftime("%Y-%m-%dT%H:%M")),
-                            ("product", "time-series"),
-                            ("Unit", "m" if metric else "e"),
-                            ("maxt","maxt"),
-                            ("mint","mint"),
-                            ("pop12","pop12")
-                            ]
 
-  return requests.get(FORECAST_URL,params = params)
+  def __init__(self, forecast_element, lat=37.76, lon=-122.412):
 
+    self.forecast_element = forecast_element
+    self.lat = lat
+    self.lon = lon
 
-def parse_forecast(ndfd_xml_str,forecast_element):
-  # parse forecast data
+    if self.forecast_element in self.ELEMENT_ABBR.keys():
+      self.refresh()
+    else:
+      raise ValueError('Unsupported forecast element: ' + forecast_element)
 
-  root = etree.fromstring(ndfd_xml_str)
-  el = root.find('./data/parameters/{0}'.format(forecast_element))
+  def refresh(self):
+    '''Acquire updated data from the NDFD.
+    '''
+    ndfd_xml = self._download_forecast()
+    self.forecasts = self._parse_forecast(ndfd_xml)
 
-  # get start and end time series
-  time_key = el.attrib['time-layout']
-  times = root.xpath('./data/time-layout/layout-key[text() = "{0}"]'.format(time_key))[0].getparent()
-  start_times = times.xpath('./start-valid-time/text()')
-  end_times = times.xpath('./end-valid-time/text()')
+  def _download_forecast(self):
+    '''
+    Returns the response object containing a forecast for the given lat/lon 
+    coords as XML.
+    '''
 
-  # get forecast values 
-  forecast_values = el.xpath('./value/text()')
+    start_date = datetime.datetime.today()
+    num_days = 2
+    metric = True
+    location_info = [("lat", self.lat), ("lon", self.lon)]
+    params = location_info + [("begin", start_date.strftime("%Y-%m-%dT%H:%M")),
+                              ("end", (start_date + datetime.timedelta(num_days)).strftime("%Y-%m-%dT%H:%M")),
+                              ("product", "time-series"),
+                              ("Unit", "m" if metric else "e"),
+                              (self.ELEMENT_ABBR[self.forecast_element], self.ELEMENT_ABBR[self.forecast_element])
+                              ]
 
-  # combine times and forecast values into a dictionary of forecasts
-  forecasts = []
-  for idx,val in enumerate(forecast_values):
-    forecasts.append({
-      'start_time': dp.parse(start_times[idx]), 
-      'end_time': dp.parse(end_times[idx]),
-      'value': int(val)
-      })
+    req = requests.get(self.FORECAST_URL, params = params)
+    logging.info(req.url)
 
-  return forecasts
+    return req.content
 
 
-def _weighted_mean(forecasts,time):
-  
-  for idx in range(len(forecasts)):
-    if forecasts[idx]['start_time'] < time < forecasts[idx]['end_time']:
-      time_in_current_window = (forecasts[idx]['end_time'] - time).total_seconds()
-      time_in_next_window = 12*60*60 - time_in_current_window
+  def _parse_forecast(self, ndfd_xml_str):
+    '''
+    Returns a list of forecast dictionaries parsed from the given NDFD XML 
+    string.
+    '''
 
-      p_in_current_window = (time_in_current_window/(12*60*60)) * forecasts[idx]['value']
-      p_in_next_window = (time_in_next_window/(12*60*60)) * forecasts[idx+1]['value']
-      
-      return p_in_current_window + p_in_next_window
+    root = etree.fromstring(ndfd_xml_str)
+    elm = root.find('./data/parameters/{0}'.format(self.forecast_element))
+
+    # get start and end time series
+    time_key = elm.attrib['time-layout']
+    times = root.xpath('./data/time-layout/layout-key[text() = "{0}"]'.format(time_key))[0].getparent()
+    start_times = times.xpath('./start-valid-time/text()')
+    end_times = times.xpath('./end-valid-time/text()')
+
+    # get forecast values 
+    forecast_values = elm.xpath('./value/text()')
+
+    # combine times and forecast values into a dictionary of forecasts
+    forecasts = []
+    for idx, val in enumerate(forecast_values):
+      forecasts.append({
+        'start_time': dp.parse(start_times[idx]), 
+        'end_time': dp.parse(end_times[idx]),
+        'value': int(val)
+        })
+
+    return forecasts
 
 
-def forecast_at_time(forecasts,time,forecast_element):
+  def _weighted_mean(self, time):
+    '''
+    Returns the weighted mean of two consecutive forecasts when both overlap 
+    the 12 hours following the given time.
+    '''
+    
+    for idx in range(len(self.forecasts)):
+      if self.forecasts[idx]['start_time'] <= time < self.forecasts[idx]['end_time']:
 
-  p_next_12h = None
-  if forecast_element == 'probability-of-precipitation':
-    p_next_12h = _weighted_mean(forecasts,time)
-  return p_next_12h
+        time_in_current_window = (self.forecasts[idx]['end_time'] - time).total_seconds()
+        time_in_next_window = 12*60*60 - time_in_current_window
+
+        p_in_current_window = (time_in_current_window/(12*60*60)) * self.forecasts[idx]['value']
+        p_in_next_window = (time_in_next_window/(12*60*60)) * self.forecasts[idx+1]['value']
+
+        logging.debug('First period forecast: ' + str(self.forecasts[idx]['value']))
+        logging.debug('Second period forecast: ' + str(self.forecasts[idx+1]['value']))
+        
+        return p_in_current_window + p_in_next_window
+
+
+  def forecast_at_time(self, time):
+    '''
+    Returns the 12-hour forecast beginning from the given time.
+    
+    If the given time does not fall exactly at the beginning of a forecast
+    period, the returned forecast will be interpolated using the period
+    containing the given time and the following period.
+    '''
+
+    p_next_12h = None
+    if self.forecast_element == 'probability-of-precipitation':
+      p_next_12h = self._weighted_mean(time)/100
+    return p_next_12h
+
+
+def main():
+  '''Get the probability of precipitation over the next 12 hours.
+  '''
+
+  ndfd = NDFD('probability-of-precipitation')
+  print ndfd.forecast_at_time(datetime.datetime.now(pytz.utc))
 
 
 if __name__ == '__main__':
-
-  response = download_forecast()
-  forecasts = parse_forecast(response.content,'probability-of-precipitation')
-  print forecast_at_time(forecasts,datetime.datetime.now(pytz.utc),'probability-of-precipitation')
+  main()
   
--- ndfd_test.py
+++ ndfd_test.py
@@ -1,21 +1,69 @@
-import unittest
-import ndfd
+from ndfd import NDFD
 import datetime
 import dateutil.parser as dp
 
+import unittest
+import logging
+from mock import MagicMock
+
+
 class TestNDFD(unittest.TestCase):
+
 
   def test_parse_forecast(self):
     # Test NDFD XML parsing.
 
     with open('test-forecast.xml','r') as xml_file:
+
+      # set up test data
       xml_str = xml_file.read()
-      actual = ndfd.parse_forecast(xml_str,'probability-of-precipitation')[0]
-      print actual
+      ndfd_mock = MagicMock(forecast_element='probability-of-precipitation')
+      ndfd_mock._parse_forecast = NDFD.__dict__['_parse_forecast']
+
+      # run test operation
+      actual = ndfd_mock._parse_forecast(ndfd_mock,xml_str)[0]
+
       expected = {
           'start_time': dp.parse('2013-04-06T05:00:00-07:00'), 
           'end_time': dp.parse('2013-04-06T17:00:00-07:00'), 
           'value': 17
           }
-      print expected
-      self.assertTrue(actual == expected)
+      self.assertEquals(actual, expected)
+
+
+  def test_weighted_mean(self):
+    # Test weighted-mean interpolation over two forecast periods. 
+
+      # set up test data
+      ndfd_mock = MagicMock(forecasts=[
+        {
+          'start_time': dp.parse('2013-04-06T05:00:00-07:00'), 
+          'end_time': dp.parse('2013-04-06T17:00:00-07:00'), 
+          'value': 10
+        },
+        {
+          'start_time': dp.parse('2013-04-06T17:00:00-07:00'), 
+          'end_time': dp.parse('2013-04-07T05:00:00-07:00'), 
+          'value': 20
+        }
+        ])
+      ndfd_mock._weighted_mean = NDFD.__dict__['_weighted_mean']
+
+      # test for a weighted mean over two forecast periods
+      actual = ndfd_mock._weighted_mean(ndfd_mock, dp.parse('2013-04-06T11:00:00-07:00'))
+
+      expected = 15
+      self.assertEqual(actual, expected)
+
+      # test for a weighted mean over one forecast period
+      actual = ndfd_mock._weighted_mean(ndfd_mock, dp.parse('2013-04-06T05:00:00-07:00'))
+
+      expected = 10
+      self.assertEqual(actual, expected)
+
+      # test for a weighted mean without enough remaining data 
+      self.assertRaises(
+        IndexError,
+        ndfd_mock._weighted_mean,
+        ndfd_mock, dp.parse('2013-04-06T17:00:01-07:00')
+        )
